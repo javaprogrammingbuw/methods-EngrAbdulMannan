13a5415db7cd62b86bc9eed8940f364ba0dd020f
abdulmannan <abdulmannancc33@gmail.com>
2019-03-19 15:15:07 +0100

    tasks 1 and 2

--- Prime.java
+++ Prime.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 
 public class Prime {
 		
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter a number ");
+		int n =scan.nextInt();
+		System.out.println("The number is prime: "+ isPrime( n));
+		int doubletteFirst = getPrimeDoublette(n);
+		System.out.println("The first doublette after "+n+" is: ("+doubletteFirst+", "+(doubletteFirst+2)+")" );
+		
 	//todo: write a method isPrime which tells you if a given number is a prime or not
 	//then, write another method findDoublette which takes an integer value as input and 
 	//is looking for the first prime-doublette it findes. A prime-doublette consists of
@@ -12,4 +20,26 @@ public class Prime {
 	//the function should return 0.
 		
 	
+}
+	public static boolean isPrime(int n ) {
+		for (int i =2; i<n; i++) {
+			if( n%i == 0 ) {
+				return false;
+			}
+		}
+		
+		return true;	
+	}
+	public static int getPrimeDoublette(int n ) {
+		int start = n;
+		if( start % 2 == 0 ) {
+			start++;
+		}
+		
+		for( int i = start; true; i = i+2 ) {
+			if( isPrime(i) && isPrime(i+2) ) {
+				return i;
+			}
+		}
+	}
 }
